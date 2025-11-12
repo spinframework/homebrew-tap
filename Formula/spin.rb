5af5b5e6f15ab7611652ed2501173abb2a578a6b
spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
2025-11-12 20:46:22 +0000

    chore(spin.rb): bump Spin to v3.5.1
    Signed-off-by: spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>

--- Formula/spin.rb
+++ Formula/spin.rb
@@ -1,26 +1,26 @@
 class Spin < Formula
   desc "Open-source tool for building and running serverless WebAssembly applications"
   homepage "https://github.com/spinframework/spin"
-  version "3.5.0"
+  version "3.5.1"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/spinframework/spin/releases/download/v#{version}/spin-v#{version}-macos-amd64.tar.gz"
-    sha256 "176c20cb27f42f60a2e4c5745500f90a78dc92e75b82f404a4d6d9508930f467"
+    sha256 "23767542a22ed22241656aba7d45a8b867d363336bff08fed1415fab4c12cb86"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/spinframework/spin/releases/download/v#{version}/spin-v#{version}-macos-aarch64.tar.gz"
-    sha256 "f4e9efcec7365089e3817f4079630536fa54bf66388b9c1447acf7220cabcf2d"
+    sha256 "00bd535093b98d23618df67facba3d0e8db6a2a561a4baa245d1a7bd56aabe88"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/spinframework/spin/releases/download/v#{version}/spin-v#{version}-linux-amd64.tar.gz"
-    sha256 "4d6593eb41fc2ad665253458f919d2da93fbaf478a043cc6566f3dd2e19db475"
+    sha256 "07e2ca5caec315a92214d75a9daa8c786c677f1a0423871a36dc67c9911c5a04"
   end
 
   if OS.linux? && Hardware::CPU.arm?
     url "https://github.com/spinframework/spin/releases/download/v#{version}/spin-v#{version}-linux-aarch64.tar.gz"
-    sha256 "30167f020533e5d83cb458f8abe6a59f0947bb777afeb7d8664f41d6362719bc"
+    sha256 "14c6083b59a212e967cfd96882d5f16aeda622e475cd2f28086d380ccc741f24"
   end
 
   def install
